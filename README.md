08ffe1c87921b1fd9d575cf87031d34290b42bff
Protonic2019 <sander.nobel@protonmail.com>
2019-09-28 00:03:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-bcrxop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-bcrxop)
---
+++ index.js
@@ -0,0 +1,16 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+  output.push(count);
+  count++
+
+  console.log(output);
+}
+
+fizzBuzz();
